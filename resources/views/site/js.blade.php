0f3bb45a32731c35907c7902daac0a5532caf0ed
abdelftahelTop <abdo@gamil.com>
2021-02-10 11:45:13 +0200

    the created new project for meditech and translate

---
+++ resources/views/site/js.blade.php
@@ -0,0 +1,18 @@
+
+<!--Scroll to top-->
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="js/jquery.paroller.min.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/nav-tool.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/swiper.min.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/script.js"></script>
+<script src="js/color-settings.js"></script>
